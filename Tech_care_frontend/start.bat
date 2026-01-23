03c17d99695021250eb802c88e39a4d18b00f1b6
Raveen S.Wickramasinghe <raveesulo@gmail.com>
2026-01-23 20:19:24 +0530

    Delete Tech_care_frontend directly to upload updated one

--- Tech_care_frontend/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting TechCare React Application...
-echo.
-echo Opening development server at http://localhost:5173
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-npm run dev
